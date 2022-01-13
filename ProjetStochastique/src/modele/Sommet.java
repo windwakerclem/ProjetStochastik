7a2a7775695f852cd864d30d8a0cc3b73b60bf18
windwakerclem <roblin.clement@sfr.fr>
2022-01-13 16:54:44 +0100

    ajout Sommet

---
+++ ProjetStochastique/src/modele/Sommet.java
@@ -0,0 +1,38 @@
+package modele;
+
+public class Sommet {
+	
+	private String name;
+	private int x;
+	private int y;
+	
+	
+	public Sommet(String name, int x, int y) {
+		this.name=name;
+		this.x=x;
+		this.y=y;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getX() {
+		return name;
+	}
+	
+	public String getY() {
+		return name;
+	}
+	
+	
+	public void setX(int x) {
+		this.x=x;
+	}
+	
+	public void setY(int y) {
+		this.y=y;
+	}
+
+}
