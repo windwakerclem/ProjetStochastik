b4fcc0c9b4aa64bb072dad82e7759aef7e88a15c
windwakerclem <roblin.clement@sfr.fr>
2022-01-13 19:40:08 +0100

    arc et sommet

--- ProjetStochastique/src/modele/Arc.java
+++ ProjetStochastique/src/modele/Arc.java
@@ -1,5 +1,20 @@
 package modele;
 
 public class Arc {
+	
+	private Sommet s1;
+	private Sommet s2;
+	private double cout;
+	
+	
+	public Arc( Sommet s1, Sommet s2) {
+		this.s1=s1;
+		this.s2=s2;
+		cout = Math.sqrt ( Math.pow((s2.getY()-s1.getY()),2) - Math.pow((s2.getX()-s1.getX()),2));
+	}
+	
+	public double getCout() {
+		return cout;
+	}
 
 }
--- ProjetStochastique/src/modele/Sommet.java
+++ ProjetStochastique/src/modele/Sommet.java
@@ -3,11 +3,11 @@ package modele;
 public class Sommet {
 	
 	private String name;
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 	
 	
-	public Sommet(String name, int x, int y) {
+	public Sommet(String name, double x, double y) {
 		this.name=name;
 		this.x=x;
 		this.y=y;
@@ -18,20 +18,20 @@ public class Sommet {
 		return name;
 	}
 	
-	public String getX() {
-		return name;
+	public double getX() {
+		return x;
 	}
 	
-	public String getY() {
-		return name;
+	public double getY() {
+		return y;
 	}
 	
 	
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x=x;
 	}
 	
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y=y;
 	}
 
