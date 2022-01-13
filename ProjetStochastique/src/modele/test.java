af824254ac2632c864d7a4af100d8e9dbb31e6e4
windwakerclem <roblin.clement@sfr.fr>
2022-01-13 16:47:56 +0100

    initialisation

---
+++ ProjetStochastique/src/modele/test.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class test {
+	
+	public test() {
+		
+	}
+	 public static void main(String[] args){
+		    for(int i = 0; i < 10; i++)
+		      System.out.println("test");
+		  }
+	
+
+}
